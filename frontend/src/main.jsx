908ecaba7873272971681d6400f5e1061265d658
Benjamin Cates <bjc2dogs@gmail.com>
2023-11-10 01:23:57 -0800

    Add unified header

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -64,6 +64,21 @@ This code renders our project so it can be viewed in a browser.
 */
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <header>
+      <div className = "hTitle"><a href="/">Petrpedia</a></div>
+      <a className = "headerEle" href="/new_template">
+          <img className = "circle" src = "https://static.thenounproject.com/png/2729266-200.png" height = "40"></img>
+      </a>
+      <a className = "headerEle" href="/trade">
+          <img className = "circle" src = "https://cdn.iconscout.com/icon/free/png-256/free-switch-1470433-1244947.png" height = "50"></img>
+      </a>
+      <a className = "headerEle" href="#login">
+          <img className = "circle" src = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" height = "40"></img>
+      </a>
+      <input className = "headerEle" type="text" placeholder="Search.."></input>
+    </header>
+    <div className="whole_page">
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>
 );
